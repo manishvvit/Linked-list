d426608621651a8f862fa642fc2d7e9ce9a09392
manish <manishnitcalicut@gmail.com>
2024-01-17 23:13:35 +0530

    first commit

---
+++ Nodecreation.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data ;
+    Node *next;
+    //Node next;
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+    
+
+};
+int main(){
+    Node* first=new Node(12);//static object creation
+
+    
+
+
+
+    return 0;
+}
